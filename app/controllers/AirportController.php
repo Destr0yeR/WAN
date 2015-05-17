c53f181cf770b389ee6e1cc0c8df1756325a4183
Javier <javier.palomino@tektonlabs.com>
2015-05-16 19:24:23 -0500

    fix status code

--- app/controllers/AirportController.php
+++ app/controllers/AirportController.php
@@ -3,23 +3,35 @@
 class AirportController extends BaseController {
 
 	public function search(){
-		$input = Input::all();
 
-		$results = Airport::whereHas('city.country', function($q)use($input){
-			$q 	->where('countries.name', 'LIKE','%'.$input['search_text'].'%')
-				->orWhere('cities.name', 'LIKE', '%'.$input['search_text'].'%');
-		})->orWhere('name', 'LIKE', '%'.$input['search_text'].'%')->get();
-
-		$airports = array();
-
-		foreach ($results as $result) {
-			$airports[] = $this->format_aiport($result);
-		}
-		$response['airports'] = $airports;
 		$response['status'] = array();
-		$response['status']['code'] = 200;
-		$response['status']['message'] = 'OK';
-		$response['status']['description'] = 'Operation successful';
+
+		$input = Input::all();
+		
+		if(isset($input['search_text']))
+		{
+			$results = Airport::whereHas('city.country', function($q)use($input){
+				$q 	->where('countries.name', 'LIKE','%'.$input['search_text'].'%')
+					->orWhere('cities.name', 'LIKE', '%'.$input['search_text'].'%');
+			})->orWhere('name', 'LIKE', '%'.$input['search_text'].'%')->get();
+
+			$airports = array();
+
+			foreach ($results as $result) {
+				$airports[] = $this->format_aiport($result);
+			}
+			
+			$response['airports'] = $airports;
+			$response['status']['code'] = 200;
+			$response['status']['message'] = 'OK';
+			$response['status']['description'] = 'Operation successful';
+		}
+		else
+		{
+			$response['status']['code'] = 400;
+			$response['status']['message'] = 'Bad request';
+			$response['status']['description'] = 'Missing parameter.';
+		}
 		return Response::json($response,$response['status']['code']);
 	}
 
